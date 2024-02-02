0272a308b5d951bfa5fe240f525a2b076f152ebb
Shreeja Manandhar <manandharja@gmail.com>
2024-02-02 22:50:43 +0545

    miration all complete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Counter::factory(1)->create();
+        // \App\Models\Counter::factory(1)->create();
+        // \App\Models\Product::factory(4)->create();
+        // \App\Models\Customer::factory(4)->create();
+        // \App\Models\Invoice::factory(4)->create();
+        // \App\Models\InvoiceItem::factory(5)->create();
     }
 }
